86b6a21a48dcd22562f65b5ebd18eca61bd34c21
everl0ve <wangzifan_wzf@hotmail.com>
2018-10-25 20:17:49 +0800

    Update LightGBM.py

--- LightGBM.py
+++ LightGBM.py
@@ -1,7 +1,26 @@
-import lightgbm as lgb
-import pandas as pd
-import datetime
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+import sys,os
+import xgboost as xgb
+import lightgbm as lgb
+import datetime
+from  sklearn import metrics
+from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
+from xgboost import plot_importance
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import scale
+import joblib
+import gc
+# 计算分类正确率
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 def getweekday(x):
     '''
     :param x: YYMMDD
@@ -15,69 +34,63 @@ def getweekday(x):
     weekday = dt.weekday()
     return weekday
 
-
 def create_feature(data):
     data["size"] = data["C15"].str.cat(data["C16"], sep="_")
     # 将hour列拆分为
     data["hour1"] = data["hour"].map(lambda x: str(x)[6:8])
     data["day"] = data["hour"].map(lambda x: str(x)[4:6])
     data["weekday"] = data["hour"].map(lambda x: getweekday(x))
-    #data["time_period"] = data["hour1"].map(lambda x:time_period(x))
     data["app_site_id"] = data["app_id"] + "_" + data["site_id"]
     data["app_site_id_model"] = data["app_site_id"] + "_" + data["device_model"]
     #此处可以考虑将组合特征的源特征删掉，对比效果
-    data = data.drop(["id", "hour"], axis=1)
+    data = data.drop(["id", "hour", "C15", "C16"], axis=1)
     return data
 
+datapath = "/data/sujianyu/ctrsample"
+output = "output"
+trainfile = os.path.join(datapath ,"train_sample.csv")
 
-gbm=None
-
-# 第二步，流式讀取數據(每次100萬)
-i=1
-file = "/data/barnett007/ctr-data/train.csv"
-for sub_data in pd.read_csv(file, chunksize=1000000,dtype={"C15":str,"C16":str}):
-    sub_data = create_feature(sub_data)
-    x_cols = [c for c in sub_data.columns if c not in ["click"]]
-    sub_data = sub_data.sample(frac=1.0)  # 全部打乱
-    cut_idx = int(round(0.1 * sub_data.shape[0]))
-    df_test, df_train = sub_data.iloc[:cut_idx], sub_data.iloc[cut_idx:]
-
-    # 區分特徵x和結果Y
-    x_data = df_train[x_cols]
-    y_data = df_train["click"]
-
-    # 創建lgb的數據集
-    lgb_train = lgb.Dataset(x_data, y_data.values)
-    lgb_eval = lgb.Dataset(df_test[x_cols], df_test["click"].values, reference=lgb_train)
-
-    # 第三步：增量訓練模型
-    # 重點來了，通過 init_model 和 keep_training_booster 兩個參數實現增量訓練
-    params = {
+df_reader = pd.read_csv(trainfile,chunksize=300000,dtype={"C15":str,"C16":str})
+gbm = None
+params = {
         'task': 'train',
-        'application': 'binary',
+        'application': 'regression',
         'boosting_type': 'gbdt',
-        'learning_rate': 0.01,
+        'learning_rate': 0.2,
         'num_leaves': 31,
         'tree_learner': 'serial',
         'min_data_in_leaf': 100,
         'metric': ['l1','l2','binary_logloss'],  # l1:mae, l2:mse
         'max_bin': 255,
-        'num_trees': 300,
-        'categorical_feature' : x_cols
+        'num_trees': 300
     }
+i=1
+for df_train in df_reader:
+    df_train = create_feature(df_train)
+    y_all = df_train["click"]
+    df_train = df_train.drop(["click"],axis=1)
+    #lightgbm 模型可以对类别型特征
+    columns = df_train.columns.tolist()
+    print(columns)
+    X_all = df_train.values
+    #print(X_all.shape)
+    #print(y_all)
+    #划分数据集
+    print("split dataset")
+    x_train, x_val, y_train, y_val = train_test_split(X_all, y_all, test_size = 0.2, random_state = 2018)
+    lgb_train = lgb.Dataset(x_train,y_train)
+    lgb_eval = lgb.Dataset(x_val,y_val)
     gbm = lgb.train(params,
                     lgb_train,
                     num_boost_round=1000,
                     valid_sets=lgb_eval,
-                    init_model=gbm,             # 如果gbm不爲None，那麼就是在上次的基礎上接着訓練
-                    feature_name=x_cols,
+                    init_model=gbm,  # 如果不为空，就是继续上次的训练。
+                    feature_name=columns,
                     early_stopping_rounds=10,
                     verbose_eval=False,
-                    keep_training_booster=True) # 增量訓練 
-
-    # 輸出模型評估分數
-    score_train = dict([(s[1], s[2]) for s in gbm.eval_train()])
-    score_valid = dict([(s[1], s[2]) for s in gbm.eval_valid()])
-    print('當前模型在訓練集的得分是：mae=%.4f, mse=%.4f, binary_logloss=%.4f'%(score_train['l1'], score_train['l2'], score_train['binary_logloss']))
-    print('當前模型在測試集的得分是：mae=%.4f, mse=%.4f, binary_logloss=%.4f' % (score_valid['l1'], score_valid['l2'], score_valid['binary_logloss']))
-    i += 1
+                    keep_training_booster=True)  # 增量训练
+    score_train = dict([(s[1],s[2]) for s in gbm.eval_train()])
+    score_valid = dict([(s[1],s[2]) for s in gbm.eval_valid()])
+    print('mae=%.4f, mse=%.4f, binary_logloss=%.4f' % (score_train['l1'], score_train['l2'], score_train['binary_logloss']))
+    print('mae=%.4f, mse=%.4f, binary_logloss=%.4f' % (score_valid['l1'], score_valid['l2'], score_valid['binary_logloss']))
+i+=1
