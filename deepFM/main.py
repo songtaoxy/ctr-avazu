e0409b914aef29cb7431c24d897f59761c350f86
everl0ve <wangzifan_wzf@hotmail.com>
2018-10-30 15:54:20 +0800

    Update main.py

--- deepFM/main.py
+++ deepFM/main.py
@@ -151,7 +151,7 @@ dfm_params = {
     "use_deep": True,
     "embedding_size": 8,
     "dropout_fm": [1.0, 1.0],
-    "deep_layers": [32,32,32],
+    "deep_layers": [32,64,32],
     "dropout_deep": [0.5, 0.5, 0.5, 0.5],
     "deep_layers_activation": tf.nn.relu,
     "epoch": 30,
@@ -160,7 +160,7 @@ dfm_params = {
     "optimizer_type": "adam",
     "batch_norm": 1,
     "batch_norm_decay": 0.999,
-    "l2_reg": 0.1,
+    "l2_reg": 0.01,
     "verbose": True,
     "eval_metric": roc_auc_score,
     "random_seed": config.RANDOM_SEED
